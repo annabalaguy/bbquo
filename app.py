a0739b61799e8f3e3b3f69cf3231a1532fc3ab9d
Anna Balaguy <annabalaguy@hotmail.co.uk>
2022-02-21 17:38:16 +0100

    create config for heroku

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from bbquo.lib import get_quote
+
+"# I am the main heading"
+
+"## I am the sub heading"
+
+"### I am the sub sub heading"
+
+123
+
+"je suis une string"
+
+st.write(get_quote())
